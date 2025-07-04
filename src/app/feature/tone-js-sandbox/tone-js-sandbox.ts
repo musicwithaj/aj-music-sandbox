48899394c8472b096251c9138e465cfd8ae7a238
AJ John <musicwithaj440@gmail.com>
2025-07-04 16:39:56 -0400

    Added basic tone js processing

--- src/app/feature/tone-js-sandbox/tone-js-sandbox.ts
+++ src/app/feature/tone-js-sandbox/tone-js-sandbox.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Synth } from 'tone';
 
 @Component({
 	selector: 'tone-js-sandbox',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
 	templateUrl: './tone-js-sandbox.html',
 	styleUrl: './tone-js-sandbox.scss'
 })
+
 export class ToneJsSandbox {
-	
+	private synth: Synth = new Synth().toDestination();
+	playNote() {
+		this.synth.triggerAttackRelease('C4', '8n');
+	}
 }
