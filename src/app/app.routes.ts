f76325b77a6cc122ea4462010421a2da8e3edc3d
AJ John <musicwithaj440@gmail.com>
2025-07-04 16:18:18 -0400

    Added dedicated home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ToneJsSandbox } from './feature/tone-js-sandbox/tone-js-sandbox';
+import { HomePageComponent } from './feature/home/home';
 
 export const toneJsRoute: string = 'tone-js';
+export const homePageRoute: string = 'home';
 
 export const routes: Routes = [
-	{ path: toneJsRoute, component: ToneJsSandbox }
+	{ path: '', redirectTo: homePageRoute, pathMatch: 'full' },
+	{ path: homePageRoute, component: HomePageComponent},
+	{ path: toneJsRoute, component: ToneJsSandbox },
 ];
